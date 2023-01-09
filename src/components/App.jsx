fea5cf9178b384e2df6169ad1bc7721e941361e3
sofidorosh <108129293+anenkosofi@users.noreply.github.com>
2023-01-09 17:42:38 +0200

    Rewrite function's state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,31 +7,17 @@ import { Statistics } from './Statistics';
 import { Notification } from './Notification';
 
 export function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const options = ['good', 'neutral', 'bad'];
+  const [options, setOptions] = useState({ good: 0, neutral: 0, bad: 0 });
 
   const leaveFeedback = option => {
-    switch (option) {
-      case 'good':
-        setGood(good + 1);
-        break;
-
-      case 'neutral':
-        setNeutral(neutral + 1);
-        break;
-
-      case 'bad':
-        setBad(bad + 1);
-        break;
-
-      default:
-        return;
-    }
+    setOptions(prevOptions => ({
+      ...prevOptions,
+      [option]: options[option] + 1,
+    }));
   };
 
   const countTotalFeedback = () => {
+    const { good, neutral, bad } = options;
     return good + neutral + bad;
   };
 
@@ -39,21 +25,24 @@ export function App() {
     if (!countTotalFeedback()) {
       return 0;
     }
-    return Math.round((good / countTotalFeedback()) * 100);
+    return Math.round((options.good / countTotalFeedback()) * 100);
   };
 
   return (
     <Box>
       <GlobalStyle />
       <Section title="Please leave feedback">
-        <FeedbackOptions options={options} onLeaveFeedback={leaveFeedback} />
+        <FeedbackOptions
+          options={Object.keys(options)}
+          onLeaveFeedback={leaveFeedback}
+        />
       </Section>
       <Section title="Statistics">
         {countTotalFeedback() ? (
           <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
+            good={options.good}
+            neutral={options.neutral}
+            bad={options.bad}
             total={countTotalFeedback()}
             positivePercentage={countPositiveFeedbackPercentage()}
           />
